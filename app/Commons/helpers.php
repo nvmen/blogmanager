65d23a8854404dcbf3ab07a5de15afdff707c565
Men Nguyen <nvmen@tma.com.vn>
2018-03-23 17:15:56 +0700

    aa

--- app/Commons/helpers.php
+++ app/Commons/helpers.php
@@ -7,3 +7,4 @@
  */
 
 define('GET_NEW_USER_REGISTER', 'https://letup.com.vn/wp-json/wp/v2/users/allusers');
+define('GET_NEW_UPDATE_STATUS_USER', 'https://letup.com.vn/wp-json/wp/v2/users/updateuser');
